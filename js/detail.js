81cbef42609146f001e07850dbcc649ff551badb
shyxyz2012 <3038107648@qq.com>
2018-01-24 15:22:04 +0800

    lu

--- js/detail.js
+++ js/detail.js
@@ -43,7 +43,6 @@ $(function(){
         $(this).css("border","2px solid #000").siblings().css("border","2px solid transparent");
     });
 
-
     $(".dLeft-big").mousemove(function(e){
         //获取鼠标的位置
         var x= e.pageX;
@@ -56,6 +55,19 @@ $(function(){
         var bigx=x-bigL-zoomW/2;
         var bigy=y-bigT-zoomH/2;
         $(".zoom").css({"left":bigx+"px","top":+bigy+"px"});
+
+        var zoomL=parseInt($(".zoom").css('left'));
+        var zoomT=parseInt($(".zoom").css('top'));
+        if(zoomL<=0){
+            $(".zoom").css('left',0);
+        }else if(zoomL>=430){
+            $(".zoom").css('left','230px');
+        }
+        if(zoomT<=0){
+            $(".zoom").css('top',0);
+        }else if(zoomT>=300){
+            $(".zoom").css('top','300px');
+        }
     });
     //商城图片放大效果部分结束
 
@@ -93,3 +105,34 @@ $(function(){
     }
     //幻灯片部分结束
 });
+
+//大家都在买部分点击箭头图片滑动开始
+//点击右箭头切换
+//"outerWidth"  默认包括元素的内边距(padding)、边框(border)，但不包括外边距(margin)部分的高/宽度
+$(function(){
+    var $small_width=$(".shop-list li").outerHeight(true);
+    var $suojin=0;
+    var $zong_suo=3;
+    $(".arrow-right").bind("click",function () {
+        if ($suojin==$zong_suo){
+            $suojin=0;
+            $(".shop-list").stop(true,true).animate({top:"0"},500);
+        }else {
+            $suojin++;
+            $(".shop-list").stop(true,true).animate({top:"-="+$small_width},500);
+        }
+    });
+
+//点击左箭头切换图片
+    $(".arrow-left").bind("click",function () {
+        if ($suojin==0){
+            $suojin=$zong_suo;
+            $(".shop-list").stop(true,true).animate({top:"-="+$zong_suo*$small_width},500);
+        }else {
+            $suojin--;
+            $(".shop-list").stop(true,true).animate({top:"+="+$small_width},500);
+        }
+    });
+})
+
+//大家都在买部分点击箭头图片滑动结束
